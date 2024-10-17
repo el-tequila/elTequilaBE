939ac5b18217707e6aef90cf43b6f0be1106e945
Veronica Vijil <veronicavijil@yahoo.com>
2024-10-16 19:04:47 -0700

    backend connected

--- api/requests.route.js
+++ api/requests.route.js
@@ -4,5 +4,6 @@ import ContactsController from './contacts.controller.js';
 const router = express.Router();
 
 router.route("/contacts").post(ContactsController.apiPostContact);
+console.log ("here");
 
 export default router;
